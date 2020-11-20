46dba7c5bc00ec19762507f8110a761dfec0c370
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-11-19 22:37:34 -0500

    commit tree 82a6323d6507d3a4ef7c8cd1ff9a910dd610bb7e

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
+ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeStr){
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+
+  if (nowHour < 12){
+      return "Good Morning";
+    } else if (nowHour > 17){
+      return "Good Evening";
+    }
+    return "Good Afternoon";
+  }
+
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
